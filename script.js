187c249c1b335f0a81a9ecca26e3b13dce7b6575
Ekinsley2002 <146798945+Ekinsley2002@users.noreply.github.com>
2023-12-11 17:59:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-test
+.board {
+  display: grid;
+  grid-template-columns: repeat(5, 50px);
+  gap: 5px;
+}
+.cell {
+  width: 50px;
+  height: 50px;
+  background-color: white;
+  border: 1px solid black;
+  cursor: pointer;
+}
+.is-off {
+  background-color: black;
+}
